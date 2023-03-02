5d30fae0e8a830d8eef9398bbef7e9e79d4a64cf
luxgile <bisslit.12.2@gmail.com>
2023-03-02 23:39:39 +0100

    0.2.1 start, added egui

---
+++ examples/egui/main.rs
@@ -0,0 +1,12 @@
+use std::error::Error;
+
+use egui::RawInput;
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let egui = egui::Context
+    let raw_input = RawInput::default();
+    let output = 
+    Ok(())
+}
+
